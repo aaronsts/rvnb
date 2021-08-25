23498a0af08c0065e940ed15b4d072611209906c
aaron staes <aaron.staes@gmail.com>
2021-08-25 11:08:26 +0200

    added initial mapbox features

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,6 +4,14 @@ class ListingsController < ApplicationController
   def index
     @listings = Listing.all
     @listings = policy_scope(Listing)
+
+    @markers = @listings.geocoded.map do |listing|
+      {
+        lat: listing.latitude,
+        lng: listing.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { listing: listing })
+      }
+    end
   end
 
   def show
