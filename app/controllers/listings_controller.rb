b372e4c1c7b89406fc147bff9a7399d51b7b9913
Mert Arslan <merarslan9@gmail.com>
2021-08-27 16:07:18 +0300

    styled the map more

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -30,6 +30,7 @@ class ListingsController < ApplicationController
   def show
     @listing = Listing.find(params[:id])
     @booking = Booking.new
+    @markers = [{ lat: @listing.latitude, lng: @listing.longitude, image_url: helpers.asset_url('logo-RVnB.png'), info_window: render_to_string(partial: "info_window", locals: { listing: @listing })}]
     authorize @listing
   end
 
